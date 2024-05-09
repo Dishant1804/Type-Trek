f48f9836437ec91320a163b2f96f84a118d926b0
Dishant1804 <dishantmiyani1804@gmail.com>
2024-05-10 00:06:11 +0530

    user typings

--- src/components/CharactersMapping.tsx
+++ src/components/CharactersMapping.tsx
@@ -1,5 +1,6 @@
 import GeneratedCharacters from './GeneratedCharacters';
 import useFetchWords from '../hooks/useFetchwords';
+import UserTypings from './UserTypings';
 
 
 const CharactersMapping = () => {
@@ -9,6 +10,7 @@ const CharactersMapping = () => {
 
     return <div>
         <GeneratedCharacters characters={characters} />
+        <UserTypings characters={characters} />
     </div>
 }
 
---
+++ src/components/UserTypings.tsx
@@ -0,0 +1,15 @@
+
+interface UserTypingsProps {
+    characters : string[]
+    userInput : string
+}
+
+const UserTypings: React.FC<UserTypingsProps> = () =>{
+
+
+    return <div>
+        hello
+    </div>
+}
+
+export default UserTypings;
