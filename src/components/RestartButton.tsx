6cbe00e941c39a3438f190df775845381feb0d41
Dishant1804 <dishantmiyani1804@gmail.com>
2024-05-29 16:13:40 +0530

    code clean up

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -4,12 +4,15 @@ const RestartButton = ({restart} : {restart : ()=>void}) =>{
     const buttonRef = useRef<HTMLButtonElement>(null);
 
     const handleClick = () => {
-        // window.location.reload();
         buttonRef.current?.blur();
         restart();
     };
 
-    return <button ref={buttonRef} tabIndex={-1} onClick={handleClick} className="text-primary-text-yellow text-xl hover:bg-slate-700/50 p-2">
+    return <button
+        ref={buttonRef}
+        tabIndex={-1}
+        onClick={handleClick}
+        className="text-primary-text-yellow text-xl hover:bg-slate-700/50 p-2">
         Restart
     </button>
 }
