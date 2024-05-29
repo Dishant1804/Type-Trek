56bec6a13c7e5d6c1d4850fc8611a0a83f950ef9
Dishant1804 <dishantmiyani1804@gmail.com>
2024-05-29 18:07:01 +0530

    mobile view message

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 function App() {
   return (
     <Router>
-      <div className='flex flex-col min-h-screen overflow-hidden'>
+      <div className='flex flex-col min-h-screen min-w-screen overflow-hidden'>
         <header>
-          <nav className='pr-24 mt-2'>
-            <div className='flex justify-between text-primary-text-yellow backdrop-blur-xl rounded-2xl bg-primary-bg mx-auto z-[20] w-full top-0  items-center p-4 pl-32'>
+          <nav className='lg:pr-24 lg:pl-32 mt-2 md:mr-0 md:pr-0 md:pl-0 '>
+            <div className='flex justify-between text-primary-text-yellow backdrop-blur-xl rounded-2xl bg-primary-bg mx-auto z-[20] w-full top-0  items-center p-4 '>
               <div className='text-xl'>
                 <NavLink to='/'>TypeTrek</NavLink>
                 <FontAwesomeIcon icon={faTerminal} className='ms-2 me-2' />
--- src/components/TypingPage.tsx
+++ src/components/TypingPage.tsx
@@ -4,41 +4,67 @@ import CountdownTimer from './CountdownTimer';
 import Result from './Result';
 import RestartButton from './RestartButton';
 import Options from './Options';
+import { useState, useEffect } from 'react';
 
 
 const TypingPage = () => {
-    const {words,totalTyped , time , errorCount , setErrorCount , state , setInitialWords , typed , setTyped , restart} = useEngine();
-    
+    const { words, totalTyped, time, errorCount, setErrorCount, state, setInitialWords, typed, setTyped, restart } = useEngine();
+
+    const [showAlert, setShowAlert] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 1024) {
+                setShowAlert(true);
+            } else {
+                setShowAlert(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className='mt-32 grid place-content-center'>
-            <div>
-                <Options 
-                setInitialWords={setInitialWords}
-                setTyped={setTyped}
-                state={state}/>
-            </div>
-            <div className='mt-28'>
-                <CountdownTimer time={time} />
-            </div>
-            <div className='relative text-3xl w-auto h-[250px] leading-relaxed break-all'>
-                <CharactersMapping
-                words={words}
-                setErrorCount={setErrorCount}
-                typed={typed}/>
-            </div>
-            <div className='grid place-content-center w-[1260px]'>
-                <RestartButton 
-                restart = {restart}
-                />
-            </div>
-            <div className='mt-8 '>
-                {state === 'finish' && (
-                    <Result 
-                    errorCount={errorCount}
-                    total={totalTyped}/>
-                )}
-            </div>
-        </div> 
+            {showAlert ?
+                <div className='text-primary-text-yellow items-center h-[500px] flex '>
+                    This is a Speed Typing application <br />
+                    For a better experience, please use a Computer or Laptop
+                </div> : <>
+                <div>
+                    <Options
+                        setInitialWords={setInitialWords}
+                        setTyped={setTyped}
+                        state={state} />
+                </div>
+                <div className='mt-28'>
+                    <CountdownTimer time={time} />
+                </div>
+                <div className='relative text-3xl w-auto h-[250px] leading-relaxed break-all'>
+                    <CharactersMapping
+                        words={words}
+                        setErrorCount={setErrorCount}
+                        typed={typed} />
+                </div>
+                <div className='grid place-content-center w-[1260px]'>
+                    <RestartButton
+                        restart={restart}
+                    />
+                </div>
+                <div className='mt-8 '>
+                    {state === 'finish' && (
+                        <Result
+                            errorCount={errorCount}
+                            total={totalTyped} />
+                    )}
+                </div>
+            </>}
+        </div>
     )
 };
 
